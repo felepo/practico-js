d83bf6e18b7618320c5e8e37d8ba4e1347d14594
Fernando Poncio <fecrackle@gmail.com>
2021-07-15 23:34:24 -0600

    agregando practica basica de figuras geometricas

---
+++ README.md
@@ -0,0 +1,10 @@
+# practicas en JavaScript
+
+...
+
+## Taller #1: figuras geometricas
+
+- Primer paso: definir las formulas
+- Segundo paso: implementar las formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML
---
+++ figuras.js
@@ -0,0 +1,59 @@
+// Codigo del cuadrado
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + " cm^2");
+console.groupEnd();
+
+// Codigo del triangulo
+console.group("Triangulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del triangulo miden: " 
+    + ladoTriangulo1 
+    + " cm, " 
+    + ladoTriangulo2 
+    + " cm, " 
+    + baseTriangulo 
+    + " cm"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triangulo es de: " + alturaTriangulo + " cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perimetro del triangulo es: " + perimetroTriangulo + " cm");
+
+const areaTriangulo = ( baseTriangulo * alturaTriangulo ) / 2;
+console.log("El area del triangulo es: " + areaTriangulo + " cm^2");
+
+console.groupEnd();
+
+// Codigo del circulo
+console.group("Circlo");
+
+const radioCirculo = 4;
+console.log("El radio del circulo es de: " + radioCirculo + " cm");
+
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es de: " + diametroCirculo + " cm");
+
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perimetro del circulo es de: " + perimetroCirculo + " cm");
+
+const areaCirculo = ( radioCirculo * radioCirculo ) * PI;
+console.log("El area del circulo es de: " + areaCirculo + " cm^2");
+
+console.groupEnd();
